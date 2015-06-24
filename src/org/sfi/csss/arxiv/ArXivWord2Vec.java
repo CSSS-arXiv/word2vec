2742c51e2ef120ff55221ee5cf2fcb61a0962355
bschneeman <bschneeman@homeaway.com>
2015-06-24 08:52:11 -0600

    added initial exploration

---
+++ src/org/sfi/csss/arxiv/ArXivWord2Vec.java
@@ -0,0 +1,108 @@
+package org.sfi.csss.arxiv;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.deeplearning4j.plot.BarnesHutTsne;
+import org.deeplearning4j.text.sentenceiterator.FileSentenceIterator;
+import org.deeplearning4j.text.sentenceiterator.LineSentenceIterator;
+import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.text.sentenceiterator.SentencePreProcessor;
+import org.deeplearning4j.text.tokenization.tokenizer.TokenPreProcess;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.EndingPreProcessor;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+import org.deeplearning4j.util.SerializationUtils;
+import org.nd4j.linalg.factory.Nd4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class ArXivWord2Vec {
+
+	private static Logger log = LoggerFactory.getLogger(ArXivWord2Vec.class);
+
+
+
+	public static void main(String[] args) throws Exception {
+
+
+		// Customizing params
+		Nd4j.ENFORCE_NUMERICAL_STABILITY = true;
+		int layerSize = 300;
+
+		log.info("Load data....");
+//		ClassPathResource resource = new ClassPathResource("raw_sentences.txt");
+//		SentenceIterator iter = new LineSentenceIterator(resource.getFile());
+		SentenceIterator iter = new FileSentenceIterator(new File("/Users/bschneeman/projects/github.com/arXiv/r-exploratory/scrub2"));
+		iter.setPreProcessor(new SentencePreProcessor() {
+			@Override
+			public String preProcess(String sentence) {
+				return sentence.toLowerCase();
+			}
+		});
+
+		log.info("Tokenize data....");
+		final EndingPreProcessor preProcessor = new EndingPreProcessor();
+		TokenizerFactory tokenizer = new DefaultTokenizerFactory();
+		tokenizer.setTokenPreProcessor(new TokenPreProcess() {
+			@Override
+			public String preProcess(String token) {
+				token = token.toLowerCase();
+				String base = preProcessor.preProcess(token);
+				base = base.replaceAll("\\d", "d");
+				if (base.endsWith("ly") || base.endsWith("ing"))
+					System.out.println(base);
+				return base;
+			}
+		});
+
+		log.info("Build model....");
+		Word2Vec vec = new Word2Vec.Builder()
+		.batchSize(1000)
+		.sampling(1e-5)
+		.minWordFrequency(5)
+		.useAdaGrad(false)
+		.layerSize(layerSize)
+		.iterations(3)
+		.learningRate(0.025)
+		.minLearningRate(1e-2)
+		.negativeSample(10)
+		.iterate(iter)
+		.tokenizerFactory(tokenizer)
+		.build();
+		vec.fit();
+
+		log.info("Evaluate model....");
+		double sim = vec.similarity("people", "money");
+		log.info("Similarity between people and money: " + sim);
+		Collection<String> similar = vec.wordsNearest("day", 20);
+		log.info("Similar words to 'day' : " + similar);
+
+		log.info("Plot TSNE....");
+		BarnesHutTsne tsne = new BarnesHutTsne.Builder()
+		.setMaxIter(1000)
+		.stopLyingIteration(250)
+		.learningRate(500)
+		.useAdaGrad(false)
+		.theta(0.5)
+		.setMomentum(0.5)
+		.normalize(true)
+		.usePca(false)
+		.build();
+		vec.lookupTable().plotVocab(tsne);
+
+		log.info("Save vectors....");
+		SerializationUtils.saveObject(vec, new File("vec.ser"));
+		WordVectorSerializer.writeWordVectors(vec, "words.txt");
+
+		log.info("****************Example finished********************");
+
+	}
+
+
+}
+
+
